ed95a0f7eddac65e6d45cba9311b7532cb87dec7
karsyx <karsyx@kali-dell.kali-dell.com>
2021-11-30 04:48:14 -0300

    Criado um encurtador de URL utilizando o banco de dados MongoDB

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: "http://localhost:3000",
+    MONGO_CONNECTION: 'mongodb+srv://dbTeste:<password>@teste-dio.arfxh.mongodb.net/dbTeste?retryWrites=true&w=majority',
+};
+//# sourceMappingURL=Constants.js.map
---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+    API_URL: "http://localhost:3000",
+    MONGO_CONNECTION:
+    'mongodb+srv://<dbTeste>:<password>@teste-dio.arfxh.mongodb.net/<dbTeste>?retryWrites=true&w=majority', // coloque o link de conexão de seu banco de dados
+}
